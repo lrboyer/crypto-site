b9f434f12e6826a451b585f2ebe4a02e2c421037
lrboyer <lrboyer@clemson.edu>
2022-03-05 21:46:12 -0500

    added database user creation

--- src/components/DashBoard.tsx
+++ src/components/DashBoard.tsx
@@ -1,14 +1,43 @@
+import { useEffect, useState } from "react";
 import { LogOut } from "./LogOut";
 import { useUserAuth } from "./userAuthContext";
+import {
+  collection,
+  getDocs,
+  updateDoc,
+  doc,
+  deleteDoc,
+  setDoc,
+} from "firebase/firestore";
+import { db } from "../firebase-config";
+import { create } from "domain";
 
 export const DashBoard = () => {
   const { user } = useUserAuth();
+  let userId = user.uid;
+  let disName = user.displayName;
+  let mail = user.email;
+
+  const usersCollectionRef = collection(db, "users");
+
+  const createUser = async () => {
+    await setDoc(doc(usersCollectionRef, userId), {
+      name: disName,
+      email: mail,
+    }).catch((err) => {
+      console.log(err);
+    })
+  };
+
+  useEffect(() => {}, []);
+
   return (
     <div>
       <LogOut />
-      <p>{user.displayName}</p>
+      <button onClick={createUser}>Create Account</button>
+      <p>{disName}</p>
       <p>{user.email}</p>
-      <img src={user.photoURL}/>
+      <img src={user.photoURL} />
     </div>
   );
 };
