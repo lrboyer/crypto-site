5f20d016d754108a5bbc1bd73f9c073c0e47b812
lrboyer <lrboyer@clemson.edu>
2022-04-04 20:55:27 -0400

    finished adding profile coins that you have bought and money spent

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -4,10 +4,12 @@ import Footer from "./Footer";
 import { useState, useEffect } from "react";
 import { useUserAuth } from "./userAuthContext";
 import { db } from "../firebase-config";
+import CoinItem from "./CoinItem";
 
-export const Profile = () => {
-	const { user } = useUserAuth();
-	const [userInfo, setUserInfo] = useState({});
+export let Profile = () => {
+	let { user } = useUserAuth();
+	let [userSpent, setUserSpent] = useState(0);
+	let [userCoins, setUserCoins] = useState([]);
 
 	useEffect(() => {
 		const getUserInfo = async () => {
@@ -20,26 +22,42 @@ export const Profile = () => {
 					.catch((err) => {
 						console.log(err);
 					});
-				let data;
-				if (docSnap.exists()) data = docSnap.data();
-				setUserInfo(data);
+				if (docSnap.exists()) {
+					console.log(docSnap.data());
+					setUserSpent(docSnap.data().spent);
+					setUserCoins(docSnap.data().coins);
+				}
 			} catch (err) {
 				console.log(err);
 			}
 		};
-		getUserInfo();
-		console.log(userInfo);
+		getUserInfo().catch((error) => {
+			console.log(error);
+		});
 	}, []);
 
 	return (
-		<div className="h-auto bg-navyblue">
+		<div className="h-full w-full bg-navyblue">
 			<NavBar />
-			<div className="flex flex-col">
+			<div className="flex flex-col m-auto items-center bg-white w-1/4 align-middle">
 				<h1>Profile</h1>
-
+				<img className="h-20 w-20" src={user.photoURL} alt="" />
 				<p>{user.displayName}</p>
 				<p>{user.email}</p>
-				<img className="h-20 w-20" src={user.photoURL} alt="" />
+			</div>
+			<div className="flex flex-col mt-5 m-auto items-center bg-white w-1/2 align-middle">
+				<h1>Assets</h1>
+				{Object.entries(userCoins).map(([coin, number], i) => (
+					<div className="bg-blue-500 m-2" key={i}>
+						<span>
+							{coin}: {number}
+						</span>
+						<span>
+							
+						</span>
+					</div>
+				))}
+				<p>Money Invested: ${userSpent.toLocaleString()}</p>
 			</div>
 			<Footer />
 		</div>
